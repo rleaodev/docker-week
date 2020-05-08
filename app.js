fcbc0f8f7fc800c119e25b8ec42abeac3ed4e922
Rafael Leão <rafael.leao@hbsis.com.br>
2020-05-08 08:45:53 -0300

    Final codes

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use((req, res, next) => {
+  console.info(`Requests received in ${process.env.HOSTNAME}`)
+  next()
+})
+
 // router
 app.use('/', require('./src/routes'));
 
